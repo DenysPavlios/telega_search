f75b1ee3ded255a51ec847dade160d933dcd725b
“Denys <denys.pavlichenko@anvileight.com>
2024-11-27 14:17:50 +0200

    fix

--- test_tg_bot.py
+++ test_tg_bot.py
@@ -2,46 +2,45 @@ import os
 import requests
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 from dotenv import load_dotenv
+import time
 
 load_dotenv()
-from selenium.webdriver.chrome.options import Options
-import time
 
 def test_name():
     options = Options()
     options.add_argument('--headless')
     driver = webdriver.Chrome(options=options)
     driver.implicitly_wait(10)
-    driver.get('https://t.me/s/openqwertya')
+    driver.get('https://t.me/s/raketa_trevoga')
 
     processed_texts = set()
+    keywords = ('борты', 'пуски', 'пуск', 'борт', 'балистика', 'бпла', 'ракет')
 
     while True:
-        driver.refresh()
-        bitcoin_today = driver.find_elements(
-            By.CSS_SELECTOR, 'div.tgme_widget_message_text.js-message_text')
+        driver.refresh()  # Обновляем страницу
+        messages = driver.find_elements(By.CSS_SELECTOR, 'div.tgme_widget_message_text.js-message_text')
 
-        if bitcoin_today:
-            element_last = bitcoin_today[-1]
-            bitcoin_text = element_last.text.lower()
-            print('--------')
+        if messages:
+            last_message = messages[-1]
+            chat_text = last_message.text.lower()
 
-            if ('борты', 'пуски', 'пуск', 'борт','балистика' in bitcoin_text) and bitcoin_text not in processed_texts:
-                # Получаем токен из переменной окружения
+
+            matches = [word for word in keywords if word in chat_text]
+            if matches and chat_text not in processed_texts:
+
                 token = os.getenv('TELEGRAM_TOKEN')
                 chat_id = '178529845'
 
-                requests.post(f"https://api.telegram.org/bot{token}/sendMessage",
-                              json={'chat_id': chat_id, 'text': bitcoin_text})
 
-                processed_texts.add(bitcoin_text)
+                requests.post(
+                    f"https://api.telegram.org/bot{token}/sendMessage",
+                    json={'chat_id': chat_id, 'text': chat_text}
+                )
+
+                processed_texts.add(chat_text)
 
         time.sleep(5)
 
-
-
 test_name()
-
-
-
